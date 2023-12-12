5f88b82481202348ade3297311a498d9093e3109
Rida003 <redaessahi@gmail.com>
2023-12-12 12:31:43 +0100

    fixed betty errors

--- shell.h
+++ shell.h
@@ -1,11 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* struct declarations */
-typedef struct cmds {
-	struct cmds *next;
-	int (*run_cmd)(char *);
-} cmd_t;
+extern char **environ;
 
 /* header files */
 #include <sys/types.h>
@@ -16,5 +12,71 @@ typedef struct cmds {
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <signal.h>
+
+/**
+ * struct shell_env - stores address for pointer to free.
+ * @path_values: pointer to path values
+ * @input: pointer to buffer that getline creates.
+ * @input_token: pointers for input token.
+ * @modify_path: pointer to the path after it has been modified
+ *
+ * Description: provides a storage for all elements that are malloced.
+ */
+typedef struct shell_env
+{
+	char **path_values;
+	char *input;
+	char **input_token;
+	char *modify_path;
+} shell_t;
+
+/**
+ * struct built_in_cmd - struct for different built in commands.
+ * @cmd_name: name of the cmd
+ * @cmd: function pointer to run the cmd
+ *
+ * Description: struct for different built in commands in our shell
+ */
+typedef struct built_in_cmd
+{
+	char *cmd_name;
+	void (*cmd)(shell_t *);
+} built_t;
+
+/* main.c */
+int run_build_in(shell_t *, char *);
+int run_command(shell_t *, char *, char **);
+int run_path(shell_t *, char *);
+int check_slash(char *);
+
+/* print_env.c */
+void print_env(shell_t *);
+
+/* string.c */
+size_t _strlen(char *);
+char *_strdup(char *);
+char **tokenize_str(char *, char *);
+int _strcmp(char *, char *);
+
+/* prompt_util.c */
+void print_ps1(int);
+char *find_pathname(char **, char *);
+char *_getenv(const char *);
+char *make_pathname(char *, char *);
+char **get_path(char **);
+
+/* prompt_util2.c */
+void free_shell_t(shell_t *);
+void p_commanderr(char *, char *);
+
+/* function prototypes */
+char *_strtok(char *, const char *);
+ssize_t getline(char **, size_t *, FILE *);
+
+/* exit_env.c */
+void my_exit(shell_t *);
 
 #endif
